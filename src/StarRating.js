a6f14110457886e0176f0763cb99ac1703f4d827
Pratham <prattandon1507@gmail.com>
2024-12-19 01:23:12 +0530

    Completed Star Rating Component

--- src/StarRating.js
+++ src/StarRating.js
@@ -8,25 +8,35 @@ const containerStyle = {
 
 const starContainerStyle = {
   display: "flex",
-  gap: "4px",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
+export default function StarRating({
+  maximum = 5,
+  color = "#fcc419",
+  size = 48,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    fontSize: `${size / 1.5}px`,
+    color,
+  };
 
-export default function StarRating({ maximum = 5 }) {
   //set 5 as default if user does not pass any prop
-  const [starClick, setStarClick] = useState(0);
+  const [starClick, setStarClick] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleStar(index) {
     setStarClick(index);
+    onSetRating(index);
   }
 
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maximum }, (_, i) => (
           <span>
@@ -36,23 +46,38 @@ export default function StarRating({ maximum = 5 }) {
               key={i}
               onHoverIn={() => setTempRating(i + 1)}
               onHoverOut={() => setTempRating(starClick)}
+              color={color}
+              size={size}
+              onSetRating={onSetRating}
             />
           </span>
         ))}
       </div>
-      <p style={textStyle}>{starClick || tempRating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maximum
+          ? messages[tempRating - 1] || messages[starClick - 1] || ""
+          : tempRating || starClick || ""}
+      </p>
     </div>
   );
 }
 
-const starStyle = {
-  width: "50px",
-  height: "50px",
-  display: "block",
-  cursor: "pointer",
-};
-
-function Star({ onStar, onHoverIn, onHoverOut, full }) {
+function Star({
+  onStar,
+  onHoverIn,
+  onHoverOut,
+  full,
+  color,
+  size,
+  onSetRating,
+}) {
+  const starStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    display: "block",
+    cursor: "pointer",
+    color,
+  };
   return (
     <span
       role="button"
@@ -65,8 +90,8 @@ function Star({ onStar, onHoverIn, onHoverOut, full }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -75,7 +100,7 @@ function Star({ onStar, onHoverIn, onHoverOut, full }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+import { useState } from "react";
+
+function Test() {
+  const [movieRate, setMovieRate] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maximum={10} onSetRating={setMovieRate} />
+      <p>The movie is being rated {movieRate} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maximum={10} />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing!"]} />
+    <StarRating color="red" className="test" messages={[]} defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
